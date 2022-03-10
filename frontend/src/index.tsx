3fdf8d0a60320f61f6bae7430159142467410ad6
lucas lelievre <loucass003@gmail.com>
2022-03-10 22:46:39 +0100

    New sentry settings

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -10,6 +10,7 @@ import reportWebVitals from './reportWebVitals';
 Sentry.init({
   dsn: "https://0d2ebce6a94d40e1b12eaddfb8cb28ce@o1161627.ingest.sentry.io/6247908",
   integrations: [new BrowserTracing()],
+  environment: process.env.NODE_ENV,
   
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
