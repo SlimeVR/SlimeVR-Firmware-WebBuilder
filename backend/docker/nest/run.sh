c8b5b14f65b3aebaa1afcbf60acfcc307091958e
lucas lelievre <loucass003@gmail.com>
2022-07-23 00:33:08 +0200

    better secrets management

--- backend/docker/nest/run.sh
+++ backend/docker/nest/run.sh
@@ -1,3 +1,6 @@
+export S3_ACCESS_KEY=`cat /run/secrets/access_key`
+export S3_SECRET_KEY=`cat /run/secrets/secret_key`
+
 if [ "$APP_ENV" == "prod" ]; then
     echo "PRODUCTION ENVIRONMENT"
     npm run start
