e3b8e8f594be5d8243dbb4debbba18fe00a84557
Butterscotch! <bscotchvanilla@gmail.com>
2022-10-08 08:42:08 -0400

    Update repos/branches

--- backend/src/firmware/firmware.constants.ts
+++ backend/src/firmware/firmware.constants.ts
@@ -1,11 +1,11 @@
 // In the format of [owner, [repo, [branches]]]
 export const AVAILABLE_FIRMWARE_REPOS = new Map<string, Map<string, string[]>>([
   ['SlimeVR', new Map([['SlimeVR-Tracker-ESP', ['main']]])],
-  ['Stermere', new Map([['SlimeVR-Tracker-ESP', ['main']]])],
   ['deiteris', new Map([['SlimeVR-Tracker-ESP', ['qmc-mag-new', 'hmc-mag']]])],
   ['tianrui233', new Map([['SlimeVR-Tracker-ESP-For-Kitkat', ['qmc-axis-aligned-en', 'QMC-轴对齐-CN']]])],
   ['Lupinixx', new Map([['SlimeVR-Tracker-ESP', ['mpu6050-fifo']]])],
   ['0forks', new Map([['SlimeVR-Tracker-ESP-BMI160', ['v3dev', 'v3dev-bmm']]])],
+  ['ButterscotchV', new Map([['SlimeVR-Tracker-ESP', ['arduino-latest']]])],
 ]);
 
 export function forEachFirmwareOwner(callbackfn: (owner: string, repo: Map<string, string[]>) => void) {
