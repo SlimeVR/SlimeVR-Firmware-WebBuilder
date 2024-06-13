543732de9ca32b3c0f29e0da9b08d95a4c7e648f
loucass003 <loucass003@gmail.com>
2024-06-13 18:28:37 +0200

    CRLF to LF

--- backend/docker/nest/run.sh
+++ backend/docker/nest/run.sh
@@ -1,5 +1,5 @@
-export S3_ACCESS_KEY=`cat /run/secrets/access_key`
-export S3_SECRET_KEY=`cat /run/secrets/secret_key`
+export S3_ACCESS_KEY=$(cat /run/secrets/access_key)
+export S3_SECRET_KEY=$(cat /run/secrets/secret_key)
 
 if [ "$APP_ENV" == "prod" ]; then
     echo "PRODUCTION ENVIRONMENT"
